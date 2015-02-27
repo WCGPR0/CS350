f5ce948fa2f72fbf4b695a70b1401ba55e0881f6
Victor Wu <vwu4@cs.binghamton.edu>
2015-02-27 03:06:07 -0500

    Trying to resolve broken pipe issue

--- lab5/prj5.c
+++ lab5/prj5.c
@@ -8,7 +8,8 @@ int main (void) {
 	int N;
 	char str[100];
 	scanf("%d", &N);
-	fgets(str, sizeof(str), stdin);
+	scanf("%s", str);
+	//fgets(str, sizeof(str), stdin);
 	N--;
 
 	pid_t childPID = fork();
@@ -17,17 +18,20 @@ int main (void) {
 	if (childPID >= 0) {
 		if (childPID == 0) { //Child process
 			if (N > 0) { 
-				N--;	
+			close(fd[1]);
+	CHILD:			N--;	
 				childPID = fork();
+				if (N > 0) goto CHILD;
 			}
-			 else if (N == 0) {
-				close(fd[1]);
-				read(fd[0], str, sizeof(str));
+			 else if (N == 0) {	
+				read(fd[0], str, sizeof(str));	
+				printf("%s", str);
+				exit(0);
 			}
 		}	
 		else { //Parent process	
 			close(fd[0]);
-			write(fd[1], str, strlen(str)+1);
+			write(fd[1], str, strlen(str));
 			wait(NULL);
 			exit(0);
 		} 
